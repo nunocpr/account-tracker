e621365cbb73a052e71c1624318d9bf04eb1384e
BLACKINK\nunod <nunodacostaramos@gmail.com>
2023-07-09 22:13:54 +0100

    - Created reusable sanitize and validation functions - Created "create" functionality for transactions - Added new models for the schema (mainCategories and subCategories) (may be revised later on) - Added NextAuth and it's prisma adapter to package.json
    Next steps:
    - Implement authentication (just the functionality, excluding front end pages / components)
    - Construct CRUD functionality for each model in the schema

--- app/api/transaction/route.ts
+++ app/api/transaction/route.ts
@@ -1,11 +1,16 @@
 import prisma from '@lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
+import { sanitizeNumber, sanitizeString } from '@/lib/utils';
 
-export async function GET() {
+export async function GET(request: Request) {
+    try {
+        const transactions = await prisma?.transactions.findMany();
+        return new NextResponse(JSON.stringify({ transactions }), { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return new NextResponse('Something went wrong', { status: 500 });
+    }
 
-    const transactions = await prisma?.transactions.findMany();
-
-    return NextResponse.json({ transactions }, { status: 200 })
 }
 
 interface TransactionCreateInput {
@@ -20,11 +25,41 @@ export async function POST(request: NextRequest) {
     try {
         const json: TransactionCreateInput = await request.json();
 
-        const transaction = await prisma?.transactions.create({ data: json });
+        // validate json
+        if (
+            typeof json.type !== 'string' ||
+            typeof json.amount !== 'number' ||
+            json.description && typeof json.description !== 'string' ||
+            json.mainCategory && typeof json.mainCategory !== 'string' ||
+            json.subCategory && typeof json.subCategory !== 'string'
+        ) {
+            return new NextResponse("Invalid data format", { status: 400 });
+        }
+
+        // sanitize json
+        const sanitizedJson = {
+            type: sanitizeString(json.type),
+            amount: sanitizeNumber(json.amount),
+            description: json.description && sanitizeString(json.description),
+            mainCategory: json.mainCategory && sanitizeString(json.mainCategory),
+            subCategory: json.subCategory && sanitizeString(json.subCategory),
+        };
+
+        const transaction = await prisma?.transactions.create({
+            data: {
+                type: sanitizedJson.type,
+                amount: sanitizedJson.amount,
+                description: sanitizedJson.description,
+                mainCategory: sanitizedJson.mainCategory,
+                subCategory: sanitizedJson.subCategory,
+            },
+        });
+
         return NextResponse.json({ transaction }, { status: 200 });
+
     } catch (error) {
         console.log(error);
-        return new NextResponse("Algo se passou diéb", { status: 501 });
+        return new NextResponse("Something went wrong.", { status: 500 });
 
     }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,43 @@
 import ms from 'ms'
+import trim from 'validator/lib/trim'
+import escape from 'validator/lib/escape'
 
+/**
+ * Returns the time ago from the timestamp
+ * 
+ * @param {Date} timestamp 
+ * @param {boolean} timeOnly 
+ * @returns a string with the time ago
+ */
 export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
-  if (!timestamp) return 'never'
-  return `${ms(Date.now() - new Date(timestamp).getTime())}${
-    timeOnly ? '' : ' ago'
-  }`
+    if (!timestamp) return 'never'
+    return `${ms(Date.now() - new Date(timestamp).getTime())}${timeOnly ? '' : ' ago'}`
+}
+
+/**
+ * Returns true if the value is undefined, null, an empty object or an empty string
+ * 
+ * @param val 
+ * @returns {boolean}
+ */
+export const isEmpty = (val: any): boolean => val === undefined || val === null || (typeof val === 'object' && Object.keys(val).length === 0) || (typeof val === 'string' && val.trim().length === 0)
+
+export const sanitizeString = (input: string) => {
+    if (typeof input !== 'string') {
+        return input;
+    }
+
+    const sanitizedValue = trim(escape(input));
+
+    return sanitizedValue;
+}
+
+export const sanitizeNumber = (input: number) => {
+    if (typeof input !== 'number') {
+        return parseInt(input);
+    }
+
+    const sanitizedValue = input;
+
+    return sanitizedValue;
 }
